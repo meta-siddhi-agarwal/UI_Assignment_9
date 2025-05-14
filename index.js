8df362acebfbfc26b62cdca07bd4dc5612620174
meta-siddhi-agarwal <siddhi.agarwal@metacube.com>
2025-05-14 12:48:01 +0530

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const http = require('node:http');
+const express = require('express');
+const path = require('path');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express();
+const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+// Serve static files from your directory
+app.use(express.static(path.join(__dirname, 'parking')));
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});  
